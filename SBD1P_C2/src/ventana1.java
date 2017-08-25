60270a4239c2c360ae69d2c601646fd635096f22
pablojy <pablojy@PabloDeb.WORKGROUP>
2017-08-25 17:19:39 -0600

    Commit de tokens USQL

--- SBD1P_C2/src/ventana1.java
+++ SBD1P_C2/src/ventana1.java
@@ -7,17 +7,20 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.awt.event.ActionEvent;
 import javax.swing.JEditorPane;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Insets;
+import analizador.*;
 
 public class ventana1 extends JFrame {
 
 	private JPanel contentPane;
 	private final JEditorPane editorPane = new JEditorPane();
-
+	analizadorL1 parser =null;
 
 	/**
 	 * Create the frame.
@@ -40,6 +43,7 @@ public class ventana1 extends JFrame {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("click en boton");
+				llamadaParser();
 			}
 		});
 		panel.add(btnNewButton);
@@ -52,6 +56,49 @@ public class ventana1 extends JFrame {
 		
 		JLabel lblNewLabel_1 = new JLabel("Estatus: ");
 		contentPane.add(lblNewLabel_1, BorderLayout.SOUTH);
+		
+		
+	}
+	
+	private void llamadaParser() {
+		///llamada a las ventanas*/
+		String sentences=editorPane.getText();
+		InputStream is= new ByteArrayInputStream(sentences.getBytes());
+		
+		if (parser==null) {
+			parser= new analizadorL1(is);
+		}else {
+			analizadorL1.ReInit(is);			
+		}
+		
+		try
+	      {
+	        switch (analizadorL1.one_line())
+	        {
+	          case 0 : 
+	          System.out.print("OK. ");
+	          System.out.println("se ha aceptador "+sentences);
+	          break;
+	          case 1 : 
+	          System.out.println("Goodbye.");
+	          break;
+	          default : 
+	          break;
+	        }
+	      }
+	      catch (Exception e)
+	      {
+	        System.out.println("NOK.");
+	        System.out.println(e.getMessage());
+	        analizadorL1.ReInit(System.in);
+	      }
+	      catch (Error e)
+	      {
+	        System.out.println("Oops.");
+	        System.out.println(e.getMessage());
+	        //break;
+	      }
+		
 	}
 
 }
