f6efacb78fc2bd8028a5af6956caec3f0ec88d3f
pablojy <pablojy@PabloDeb.WORKGROUP>
2017-08-24 19:35:49 -0600

    Commit de inicio

---
+++ SBD1P_C2/src/inicioClass.java
@@ -0,0 +1,14 @@
+
+public class inicioClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos");
+		System.out.println("todos son muy bienvenidos");
+		
+		ventana1 vent = new ventana1();
+		
+		vent.show(true);
+	}
+
+}
